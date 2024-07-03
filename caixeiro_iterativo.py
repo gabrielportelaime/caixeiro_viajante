f0f121b02f543be6cd41671a8abad072c9482568
Gabriel Portela <gabriel.portela@ime.eb.br>
2024-07-03 12:50:43 -0400

    caixeiro iterativo

---
+++ caixeiro_iterativo.py
@@ -0,0 +1,12 @@
+orcamento = 10
+valores = [10, 20, 30, 40]
+matriz_adjacencia =[[0, 1, 2, 5], 
+                    [1, 0, 6, 4], 
+                    [2, 6, 0, 4], 
+                    [5, 4, 4, 0]]
+vertices = len(matriz_adjacencia)
+completos = []
+letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+S = {}
+
